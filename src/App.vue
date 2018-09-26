89abd49806b574bd74a7ed200f83289fac489c0a
Hugo Espinosa <vaporic@gmail.com>
2018-09-25 19:45:58 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 PlatziMusic
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" :value="country.value") {{country.name}}
+    spinner(v-show="loading")
+    ul
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
+
+</template>
+
+<script>
+  import getArtists from './api'
+  import Artist from './components/Artist'
+  import Spinner from "./components/Spinner";
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        artists: [],
+        countries: [
+          { name: 'Argentina', value: 'argentina'},
+          { name: 'Colombia', value: 'colombia'},
+          { name: 'España', value: 'spain'},
+          { name: 'México', value: 'mexico'},
+        ],
+        selectedCountry: 'mexico',
+        loading: true
+      }
+    },
+    components: {
+      Spinner,
+      Artist
+    },
+    methods: {
+      refreshArtist() {
+        const self = this
+        this.loading = true
+        getArtists(this.selectedCountry)
+          .then(function (artists) {
+            self.loading = false
+            self.artists = artists
+          })
+      }
+    },
+    mounted() {
+      this.refreshArtist()
+    },
+    watch: {
+      selectedCountry () {
+        this.refreshArtist()
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  #app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
+    margin-top 60px
+
+  h1, h2
+    font-weight normal
+
+  ul
+    list-style-type none
+    padding 0
+
+  li
+    display inline-block
+    margin 0 10px
+
+  a
+    color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+  appname: 'platzimusic',
+    apiKey: '666f49e403939b9dff41b7c73300a7ea',
+  secret: '777f0914c94c320be818c87e1b7a9bc8',
+  registeredTo: 'vaporic'
+}
+
+//Application name	platzimusic
+//API key	666f49e403939b9dff41b7c73300a7ea
+//Shared secret	777f0914c94c320be818c87e1b7a9bc8
+//Registered to	vaporic
